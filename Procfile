a37b8f23d2b6337ce742bb0df1fc03e99da02b01
Jonathan Wong <18wongj@gmail.com>
2018-10-13 13:24:57 -0400

    feat(and we are there, with nlp): how cool

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: python app.py
+web: gunicorn --bind 0.0.0.0:5000 --log-level=debug -w 4 app:app
--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 import os
 import json
+import ast
 import nltk
 from nltk.corpus import wordnet as wn
 import requests
-from flask import Flask, render_template, send_from_directory
+from flask import Flask, render_template, send_from_directory, Response, request, jsonify
 from flask_assets import Environment, Bundle
+import logging
 
 app = Flask(__name__, static_url_path='/static')
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
@@ -30,33 +32,46 @@ def serve_res(filename):
 def index():
     return render_template("index.html")
 
-@app.route("/api")
+@app.route("/api", methods=["GET", "POST"])
 def api():
-    topics = request.form["topics"]
-    date_range = requests.post("https://api.propublica.org/congress/v1/both/votes/2017/02");
-    feb_output = json.loads(date_range.text)
-    num_results = feb_output["results"]["num_results"]
-    votes = {}
-    for i in range(num_results):
-        vote_object = feb_output["results"]["votes"][i] 
-        votes[vote_object["question"]] = vote_object["description"]
-        # first determine relevance using wordnet (check if subject matches word or synonym)
-        for topic in topics:
-            relevant = False
-            for synset in wn.synset("topic"):
-                for lemma in synset.lemmas:
-                    if lemma.name() in vote_object["question"] or lemma.name() in vote_object["description"]:
-                        relevant = True
-                        break
-            if topic in vote_object["question"] or topic in vote_object["description"]:
-                relevant = True
-        # then determine the overall sentiment (exclude the word in this analysis possibly)
-        if relevant:
-            # use NLTK semantic analyzer
-            return ""
+    if request.method == "POST":
+        topics = json.loads(request.form["topics"])
+        date_range = requests.get("https://api.propublica.org/congress/v1/both/votes/2017/03.json", headers={"X-API-Key":"fFpjOt2GQJjCZV70ynIMuNE0yrBHlrNSQYhWZcal"})
+        mar_output = json.loads(date_range.text)
+        num_results = mar_output["results"]["num_results"]
+        results = []
+        relevant = False
+        for i in range(num_results-(num_results/10)):
+            vote_object = mar_output["results"]["votes"][i]
+            # first determine relevance using wordnet (check if subject matches word or synonym)
+            for topic in topics.iterkeys():
+                relevant = False
+                for synset in wn.synsets(topic):
+                    for l in synset.lemmas():
+                        if l.name() in vote_object["question"] or l.name() in vote_object["description"]:
+                            relevant = True
+                if topic in vote_object["question"] or topic in vote_object["description"]:
+                    relevant = True
+            # then determine the overall sentiment (exclude the word in this analysis possibly)
+            if relevant:
+                # use NLTK sentiment analysis
+                shippy = requests.get("http://text-processing.com/api/sentiment", data={"text":vote_object["question"]+""+vote_object["description"]})
+                try:
+                    sent_label = json.loads(shippy.text)["label"]
+                except:
+                    # welp no data
+                    continue
+                results.append(vote_object["question"], vote_object["description"], sent_label)
+        results = json.dumps(results)
+        app.logger.info("Dumped JSON\n"+results)
+        lan = requests.get("http://whoismyrepresentative.com/getall_mems.php?zip="+request.form["zip"]+"&output=json")
+        lan = json.loads(lan.text)
+        
 
-    return ""
+        return jsonify(results)
+    else:
+        return "method not allowed"
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8080))
-    app.run(host='0.0.0.0', port=port, debug=True) # turn debug off for production!
+    app.run(host='0.0.0.0', port=port, debug=True, threaded=True) # turn debug off for production!
